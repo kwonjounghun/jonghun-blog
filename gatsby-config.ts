2ee10244208f3f46f4d09a45a103fa0c77780fc9
권종훈 <jounghun.kwon@wadiz.kr>
2023-12-30 21:31:31 +0900

    first commit

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -8,14 +8,7 @@ const config: GatsbyConfig = {
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
-  graphqlTypegen: true,
-  plugins: [{
-    resolve: 'gatsby-source-sanity',
-    options: {
-      "projectId": "saysred6",
-      "dataset": ""
-    }
-  }, "gatsby-plugin-image", "gatsby-plugin-sharp", "gatsby-transformer-sharp"]
+  graphqlTypegen: true
 };
 
 export default config;
